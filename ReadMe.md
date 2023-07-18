e560fd9408196e08d33cf5d33f104cb075db0535
Cameron Smith <cameron.smith@kyndryl.com>
2023-07-18 18:55:14 +1000

    update

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,46 @@
-# Details on encrypting file using user and machine keys
+># tpa_log_windows
+>
+
+>## Synopsis
+>This repository contains playbooks and scripts to setup a custom Windows Event log archive solution. The playbooks create a secure encrypted password file, a schedule that runs >daily to copy archive files to a central file system, and a script to maintan the archive files. 
+---
+
+>## Playbook Overview and Execution
+
+>### Setup  
+>**CreateEncryptedPasswordFile.yml** This playbook is the setups up the encrypted password file. password string to create the file for the solution in the *C:\Kyndryl* folder, for >future reference. 
+>#### Variables
+>| Variable | Default  | Comment |
+>| --- | ---  | --- |
+>| storage_password | None | **Mandatory** This password string is required to create the creditals for running this upload process.  
+
+>### Upload schedule creation 
+>**CreateTask.yml** This playbook will create a windows task schedule that will run the *UploadArchive.ps1* PowerShell script. The Schedule Task time will be a random time between >11pm and midnight that will run daily. In the setup step the creation of the encrypted *Credential.enc* will be used as the credentials of the server user that will run the Schedule >task.  
+>#### Variables
+>| Variable | Default  | Comment |
+>| --- | ---  | --- |
+>|  
+
+>### Execution Report
+>**ExecutionReport.yml** This playbook will check the create schedule task on all hosts and produce an execution report email.
+>#### Variables
+>| Variable | Default  | Comment |
+>| --- | ---  | --- |
+>|  
+>### 180 day clean up
+>**RemoveFiles.ps1** This is a PowerShell script which removes any archive files that have a creation date greater than 180 days. The script will produce a deletion report.
+
+>#### Variables
+>| Variable | Default  | Comment |
+>| --- | ---  | --- |
+>| $StorageUser | None | **Mandatory**  User that has accesss to archive files |
+>| $Days | 180 | Define amount of days |
+>| $Targetfolder | C:\Logs | Define folder where archive event logs files are located 
+>| $Extension | *.evntx | Define extension of file to be removed 
+>| $LogDir | C:\Logs | Log file location   
+
+>### Clean Project
+>**ProjectClean.yml** This ansible playbook can be run to clean up the related Scheduled Tasks and files, that are created for this project.
+
+>### Details on encrypting file using user and machine keys
 https://stackoverflow.com/questions/23699500/convertto-securestring-gives-different-experience-on-different-servers
--- RemoveFiles.ps1
+++ RemoveFiles.ps1
@@ -5,52 +5,75 @@
 #
 # Cameron Smith 12-07-2023  Initial version 
 #-------------------------------------------------------------------------------------------------------------"
-write-host "Starting" 
+param (
+        #----- Define parameters -----#
+        [String]$Days = "180",                                                      # Define amount of days 
+        [String]$Targetfolder = "\\tdlbkpprod0201.melb.ad\wineventlogs_nonprod\",   # Define folder where archive event logs files are located 
+        [String]$Extension = "archive*.evtx",                                       # Define extension of file to be removed 
+        [String]$LogDir = "Logs",                                                   # Log file location   
+        [String]$StorageUser = "windowslogging"                                     # User that has accesss to archive files 
+)
 
-#----- Define parameters -----#
-$Days = "180"                                       # define amount of days 
-$Now = Get-Date                                     # Current Date Time
-$DisplayNow = Get-Date -format "yyyyMMdd_HHmmss"    # Date time format yyymmdd_HHmmss
-$Lastwrite = $Now.AddDays(-$Days)                   # Date and time minus amout of days  
-$Targetfolder = "C:\Logs"                           # define folder where files are located 
-$Extension = "*.txt"                                # define extension 
-$LogFile = "C:\Logs\RemoveFile_$DisplayNow.log"     # Log file for this run  
+$Now = Get-Date                                                                     # Current Date Time
+$DisplayNow = Get-Date -format "yyyyMMdd_HHmmss"                                    # Date time format yyymmdd_HHmmss
+$Lastwrite = $Now.AddDays(-$Days)                                                   # Date and time minus amout of days  
+$PasswordFile = "C:\Kyndryl\Credential.enc"                                         # Encrpted password file
 
-write-host "Date: $Now"
-write-host "Create date: $Lastwrite  (-$Days days)"
 
-#Create the log file
-#Check if file exists
-if (Test-Path $LogFile) {
-    Write-host "File '$LogFile' already exists!" -f Yellow
-}
-Else {
-    #Create a new file
-    New-Item -Path $LogFile -ItemType "File"
-    Write-host "New File '$LogFile' Created!" -f Green
-}
+# Read the encrypted password from the text file
+$securePassword = $(Get-Content -Path $PasswordFile | ConvertTo-SecureString)
+            
+# Create a PSCredential object with the network credentials
+$credential = New-Object System.Management.Automation.PSCredential($StorageUser, $securePassword)
 
-Add-Content -Path $LogFile -Value "`n`t`t --- Event Log archive delete (-$Days days) --- `n`t`t`tToday:  $DisplayNow `n _____________________________________________________________________"
-
-#----- Get files based on lastwrite filter and specified folder ---#
-$Files = Get-Childitem -Path $Targetfolder -include $Extension -Recurse | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $Lastwrite}
-write-host "Number of file found for deletion:" $Files.Count 
-
-#Check if files were found to be removed 
-if ($Files.Count -gt 0)
-{         
-    foreach ($File in $Files)
-    {
-         
-        if ($null -ne $File)
+# Go through drives and check if it is being used
+foreach ($drvletter in "ABDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray()) {
+  # Check if the drive is being used. If yes go to next letter
+  if(Get-Volume -FilePath $drvletter":\")
+    { continue }
+  
+  # Temporarily map drive
+  New-PSDrive -Name $drvletter -PSProvider FileSystem -Root $Targetfolder -Credential $credential
+    
+  #Build Log file path
+  $LogFile = "{0}:\{1}\RemoveFile_{2}.log" -f $drvletter,$LogDir,$DisplayNow  
+   
+  # Create the log file
+  #   + Check if file exists
+  if (Test-Path $LogFile) {
+      Write-host "File '$LogFile' already exists!" -f Yellow
+  }
+  else {
+      #Create a new file
+      New-Item -Path $LogFile -ItemType "File"
+      Write-host "New File '$LogFile' Created!" -f Green
+  }
+  
+  # Add header to the log file
+  Add-Content -Path $LogFile -Value "`n`t`t --- Event Log archive delete (-$Days days) --- `n`t`t`tToday:  $DisplayNow `n _____________________________________________________________________"
+  
+  # Get files based on lastwrite filter and specified folder 
+  $Files = Get-Childitem -Path $drvletter":\TestEvent" -include $Extension -Recurse | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $Lastwrite}
+  
+  #Check if files were found to be removed 
+  if ($Files.Count -gt 0)
+    {         
+        foreach ($File in $Files)
         {
-            write-host "Deleted File $File " -backgroundcolor "DarkRed"
-            $crDate = $File.CreationTime
-            Add-Content -Path $LogFile -Value "  Deleted File: $File `t Creation date: $crDate "
-            Remove-item $File.Fullname | out-null
+          if ($null -ne $File)
+            {
+              $crDate = $File.CreationTime
+              Add-Content -Path $LogFile -Value "  Deleted File: $File `t Creation date: $crDate "
+              Remove-item $File.Fullname | out-null
+            }
         }
+    }else {
+      Add-Content -Path $LogFile -Value "`n`t No files found over $Days days old."
     }
-}else {
-    write-host "No files found over $Days days old." -backgroundcolor "Green"
-    Add-Content -Path $LogFile -Value "`n`t No files found over $Days days old."
+
+  # Remove drive
+  Get-PSDrive $drvletter | Remove-PSDrive -Force -Verbose
+
+  # Exit with success return code
+  Exit 0
 }
